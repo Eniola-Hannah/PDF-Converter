11e28c84a702505e02e118038b81ebd6b59ced9f
Eniola Hannah <heniolahannah@gmail.com>
2024-05-08 11:45:45 +0100

    makes the output path to the textfield And also adds a combobox & a label to the GUI

--- src/FXMLController.java
+++ src/FXMLController.java
@@ -3,11 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -24,12 +27,25 @@ public class FXMLController implements Initializable {
     @FXML
     private Button choosefile;
     
+    @FXML
+    void handleOpenFileAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            // Process the selected file
+            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+        }
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        choosefile.setOnAction(event -> {
-            System.out.println("Button clicked!");
-            // Additional button click handling code...
-        });
+        // No need to set action for choosefile/button when there's already an handler for it up
+//        choosefile.setOnAction(event -> {
+//            System.out.println("Button clicked!");
+//            // Additional button click handling code...
+//        });
     }    
+    
     
 }
