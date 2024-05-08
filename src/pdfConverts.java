0a2191ce7f271351e53780b3b725aeb1c25e8f39
Eniola Hannah <heniolahannah@gmail.com>
2024-05-08 08:16:08 +0100

    Adds the GUI features in the FXML.fxml file

--- src/pdfConverts.java
+++ src/pdfConverts.java
@@ -3,9 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -19,20 +24,27 @@ public class pdfConverts extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-            System.out.println("Hello World!");
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            //        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction((ActionEvent event) -> {
+//            System.out.println("Hello World!");
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+
+            Parent root = FXMLLoader.load(getClass().getResource("MainFXML.fxml"));
+
+
+            Scene scene = new Scene(root, 300, 250);
+
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(pdfConverts.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
