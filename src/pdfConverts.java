2b676c9146fdb173c0f754681fc6c333741b8a1d
Eniola Hannah <heniolahannah@gmail.com>
2024-05-13 00:57:19 +0100

    Removes the unused pdftodocx file

--- src/pdfConverts.java
+++ src/pdfConverts.java
@@ -6,7 +6,6 @@
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
